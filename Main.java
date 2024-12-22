8ab3b029e0d0943d73b60d84d8ce4819c1fa559f
Sneha Shanbhag <159918012+Sneha02Shanbhag@users.noreply.github.com>
2024-12-22 08:24:27 +0530

    added main

---
+++ Main.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+import java.util.Random;
+
+class Main {
+    public static void main(String[] args){
+        Scanner reader=new Scanner(System.in);
+        String play="yes";
+        //while loop to determine if we are going to play the game again
+        while(play.equals("yes"))
+        {
+            Random rand=new Random();
+            int randNum=rand.nextInt(100);
+            int guess=-1;
+            int tries=0;
+            //while loop to check if the game is over
+            while(guess!= randNum)
+            {
+                System.out.println("Guess a number between 1 to 100:");
+                guess= reader.nextInt();
+                tries++;
+                if(guess==randNum)
+                {
+                    System.out.println("Great!! You guessed the number!");
+                    System.out.println("It only took you"  +tries+ "guesses!");
+                    System.out.println("Would you like to play again? yes or no:");
+                    play=reader.next().toLowerCase();
+                }
+                else if(guess>randNum)
+                {
+                    System.out.println("Your guess is too high, try again..");
+                }
+                else
+                {
+                    System.out.println("Your guess is to low, try again.");
+                }
+            }
+        }
+        reader.close();
+
+    }
+    
+}
